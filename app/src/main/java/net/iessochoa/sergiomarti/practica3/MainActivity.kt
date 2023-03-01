5564b5959ca391f3a686f41cb8c9010a80c36a48
Sergio Martí <sergio.martihee1997@gmail.com>
2023-03-01 12:40:11 +0100

    punto 14

--- app/src/main/java/net/iessochoa/sergiomarti/practica3/MainActivity.kt
+++ app/src/main/java/net/iessochoa/sergiomarti/practica3/MainActivity.kt
@@ -2,10 +2,26 @@ package net.iessochoa.sergiomarti.practica3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import net.iessochoa.sergiomarti.practica3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var num:Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvNumber.text = num.toString()
+
+        binding.btAddOne.setOnClickListener() {
+            num++
+            binding.tvNumber.text = num.toString()
+        }
+
     }
 }
